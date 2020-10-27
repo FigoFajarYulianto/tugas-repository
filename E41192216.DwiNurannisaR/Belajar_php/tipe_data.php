f063f02f5fdfb6f307d960a77ba5c6961994fb19
Annisahayu19 <annisahayu19@gmail.com>
2020-10-27 14:34:48 +0700

    Dwi Nurannisa

---
+++ E41192216.DwiNurannisaR/Belajar_php/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'P';
+
+// tipe data string (teks)
+$nama_lengkap = "Dwi Nurannisa R";
+
+// tipe data integer
+$umur = 20;
+
+// tipe data float
+$berat = 55;
+
+// tipe data float
+$tinggi = 155;
+
+// tipe data boolean
+$menikah = "Belum Menikah";
+
+echo "Nama : $nama_lengkap<br>";
+echo "Jenis Kelamin : $jenis_kelamin<br>";
+echo "Umur : $umur tahun<br>";
+echo "Berat Badan : $berat kg<br>";
+echo "Tinggi : $tinggi cm<br>";
+echo "Menikah : $menikah";
+?>
---
+++ E41192216.DwiNurannisaR/Belajar_php/variable.php
@@ -0,0 +1,9 @@
+<?php
+
+//membuat variabel baru
+$nama_barang = "Ice Cream";
+$harga = 8000;
+
+//menampilkan isi variabel
+echo "Saya membeli $nama_barang seharga Rp $harga";
+?>
