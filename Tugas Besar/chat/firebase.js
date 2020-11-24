04375d998d39c4dfbc4ea53d0cff2224e48a3849
Rifqi Rosyid Erdiansyah <rifqierdiansyah@gmail.com>
2020-11-24 12:19:16 +0700

    chat

---
+++ Tugas Besar/chat/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBv1bxkruVJqpkAbLmTZJ99KSBw8UmHTDI",
+    authDomain: "message-65048.firebaseapp.com",
+    databaseURL: "https://message-65048.firebaseio.com",
+    projectId: "message-65048",
+    storageBucket: "message-65048.appspot.com",
+    messagingSenderId: "391181497021",
+    appId: "1:391181497021:web:6cd6421eac1a50093b3dd4",
+    measurementId: "G-EMGZ9HKMXB"
+  };
