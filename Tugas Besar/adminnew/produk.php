f8c5d30f483859bdddefbc9fe15633749857bb55
FigoFajarYulianto <figofajar07@gmail.com>
2021-01-10 09:32:02 +0700

    figo

---
+++ Tugas Besar/adminnew/produk.php
@@ -0,0 +1,44 @@
+<h2>Daftar Produk</h2>
+
+<table class="table table-bordered" id="table">
+    <thead>
+        <tr>
+            <th>NO</th>
+            <th>NAMA PRODUK</th>
+            <th>KATEGORI</th>
+            <th>DESKRIPSI</th>
+            <th>HARGA</th>
+            <th>MAP LINK</th>
+            <th>GAMBAR</th>
+            <th>AKSI</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $nomor=1;?>
+        <?php $ambil=$koneksi->query("SELECT * FROM tb_produk"); ?>
+        <?php while($pecah=$ambil->fetch_assoc()){?>
+        <tr>
+            <td><?php echo $nomor;?></td>
+            <td><?php echo $pecah['nama_produk']?></td>
+            <td><?php echo $pecah['kategori']?></td>
+            <td><?php echo $pecah['deskripsi_produk']?></td>
+            <td><?php echo $pecah['harga']?></td>
+            <td><?php echo $pecah['map_link']?></td>
+            <td>
+                <img src="../produk2/assets/img/produk/<?php echo $pecah['gbr_produk'];?>" width="100">
+            </td>
+
+            <td>
+                <a href="index.php?halaman=hapusproduk&id=<?php echo $pecah['id_produk'];?>" class="btn btn-danger"
+                    onclick="return confirm('Yakin Mau di Hapus?')"><i class="lyphicon glyphicon-trash"></i>Hapus</a>
+                <a href="index.php?halaman=ubahproduk&id=<?php echo $pecah['id_produk'];?>" class="btn btn-warning"><i
+                        class="lyphicon glyphicon-edit"></i>Ubah</a>
+                <a href="index.php?halaman=detailproduk&id=<?php echo $pecah['id_produk'];?>" class="btn btn-info"><i
+                        class="glyphicon glyphicon-eye">Detail</a>
+            </td>
+        </tr>
+        <?php $nomor++;?>
+        <?php }?>
+    </tbody>
+</table>
+<a href="index.php?halaman=tambahproduk" class="btn btn-primary">Tambah Produk</a>
